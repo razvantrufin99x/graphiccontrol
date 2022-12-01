48c02eaaa8ed7c511fc86a748918eacfa91ad996
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:34:53 +0200

    Add project files.

---
+++ graphiccontrol/UserControl1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace graphiccontrol
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        public Graphics g;
+        public Pen pen0 = new Pen(Color.Black, 1);
+        public Brush brush0 = new SolidBrush(Color.Black);
+        public Font font0 = new Font("Arial", 10);
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+        }
+    }
+}
